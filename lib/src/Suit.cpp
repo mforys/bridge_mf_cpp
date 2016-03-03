f42523fbf8846d972cadbaf6968c3c57da85b7aa
Marek Forys <forys@o2.pl>
2016-03-03 14:22:05 +0100

    Suit class
    Signed-off-by: Marek Forys <forys@o2.pl>

---
+++ lib/src/Suit.cpp
@@ -0,0 +1,23 @@
+#include "Suit.h"
+
+Suit::Suit(suit)
+{
+    //ctor
+}
+
+Suit::~Suit()
+{
+    //dtor
+}
+
+Suit::Suit(const Suit& other)
+{
+    //copy ctor
+}
+
+Suit& Suit::operator=(const Suit& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
