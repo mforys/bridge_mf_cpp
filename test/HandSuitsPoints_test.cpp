0db93802bd2c9f3135abab61f7ca765387e959d6
Marek Foryś <Marek.Forys@tomtom.com>
2019-03-26 00:05:52 +0100

    Next test for HandSuitsPoints class added
    Signed-off-by: Marek Foryś <Marek.Forys@tomtom.com>

--- test/HandSuitsPoints_test.cpp
+++ test/HandSuitsPoints_test.cpp
@@ -12,3 +12,39 @@ TEST (HandSuitsPoints_Test, Basic) {
 
     EXPECT_EQ(12, hand.getPoints());
 }
+
+TEST (HandSuitsPoints_Test, NT_hand) {
+
+    HandSuitsPoints hand(4,3,3,3,16);
+
+    EXPECT_EQ(4, hand.getSuitCount(CLUB));
+    EXPECT_EQ(3, hand.getSuitCount(DIAMOND));
+    EXPECT_EQ(3, hand.getSuitCount(HEART));
+    EXPECT_EQ(3, hand.getSuitCount(SPADE));
+
+    EXPECT_EQ(16, hand.getPoints());
+}
+
+TEST (HandSuitsPoints_Test, OneSuit_hand) {
+
+    HandSuitsPoints hand(2,5,3,3,13);
+
+    EXPECT_EQ(2, hand.getSuitCount(CLUB));
+    EXPECT_EQ(5, hand.getSuitCount(DIAMOND));
+    EXPECT_EQ(3, hand.getSuitCount(HEART));
+    EXPECT_EQ(3, hand.getSuitCount(SPADE));
+
+    EXPECT_EQ(13, hand.getPoints());
+}
+
+TEST (HandSuitsPoints_Test, WeakTwo_hand) {
+
+    HandSuitsPoints hand(5,5,1,2,8);
+
+    EXPECT_EQ(5, hand.getSuitCount(CLUB));
+    EXPECT_EQ(5, hand.getSuitCount(DIAMOND));
+    EXPECT_EQ(1, hand.getSuitCount(HEART));
+    EXPECT_EQ(2, hand.getSuitCount(SPADE));
+
+    EXPECT_EQ(8, hand.getPoints());
+}
